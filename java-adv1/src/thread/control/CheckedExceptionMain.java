cc43e2f75619b305e38e08e82d01adf7d34a690b
hyunseok <tjgustjr33@gmail.com>
2025-12-23 16:27:15 +0900

    ✨feat: section3

---
+++ java-adv1/src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,14 @@
+package thread.control;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+        @Override
+        public void run() /*throws Exception*/ {
+            //throw new Exception();
+        }
+    }
+}

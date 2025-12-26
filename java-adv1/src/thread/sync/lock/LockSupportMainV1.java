b1fd23779d73993ba04085826df3b698e0ccce9a
hyunseok <tjgustjr33@gmail.com>
2025-12-27 02:04:01 +0900

    ✨feat: section7

---
+++ java-adv1/src/thread/sync/lock/LockSupportMainV1.java
@@ -0,0 +1,28 @@
+package thread.sync.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV1 {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTask(), "thread-1");
+        thread1.start();
+
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState());
+        LockSupport.unpark(thread1); // 1. unpark 사용
+        //thread1.interrupt(); // 2. interrupt() 사용
+    }
+
+    static class ParkTask implements Runnable {
+        @Override
+        public void run() {
+            log("park 시작");
+            LockSupport.park();
+            log("park 종료, state: " + Thread.currentThread().getState());
+            log("인터럽트 상태: " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
---
+++ java-adv1/src/util/concurrent/locks/Lock.java
@@ -0,0 +1,13 @@
+package util.concurrent.locks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+
+public interface Lock {
+    void lock();
+    void lockInterruptibly() throws InterruptedException;
+    boolean tryLock();
+    boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
+    void unlock();
+    Condition newCondition();
+}
